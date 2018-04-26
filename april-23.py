8872c33846063e2b02a16931d4ccc49a1a9b2ed9
moreKEYTAR <lantz.ek@gmail.com>
2018-04-26 23:29:11 +0000

    All files added; still cannot push

--- april-23.py
+++ april-23.py
@@ -17,7 +17,7 @@ def gradingStudents(grades):
             # using mod will determine if the value is closer to the lower multiple of 5 or higher:
             if grades[i] % 5 >= 3:
                 rounded_grade = ((grades[i] / 5) + 1) * 5
-                grades[i] =  rounded_grade
+                grades[i] = rounded_grade
     return grades
 
 #------------------------------------------#
@@ -27,19 +27,19 @@ def kangaroo(x1, v1, x2, v2):
     Values for x denote the starting location on the number line, O or higher.
     Values for v denote the distance per second for that kangaroo."""
     # 0 3 4 2
-    
+
     if v2 >= v1:  # We know x1 is always less than x2, so a faster x2 kangaroo means they will never meet
         return "NO"
     else:
         d1 = x1
         d2 = x2
         t = 0
-        
+
         while d1 < d2:
             t += 1  # good thing to track, in case it is needed in a real use case
             d1 += v1
             d2 += v2
             if d1 == d2:
                 return "YES"
-    
+
     return "NO"
